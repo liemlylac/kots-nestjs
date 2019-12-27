ad550d0cddfec7fd47b8b562aad86b6f6c03fce1
liemlylac <liemlylac@gmail.com>
2019-12-27 11:33:44 +0700

    WIP integrate testing with circle-ci

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
-import { UserEntityRepository } from '../user.entity.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from '../user.entity';
+import { UserMockRepository } from './user.mock.repository';
 
 describe('User Controller', () => {
   let userController: UserController;
@@ -10,7 +12,13 @@ describe('User Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService, UserEntityRepository],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: UserMockRepository,
+        },
+      ],
     }).compile();
 
     userController = module.get<UserController>(UserController);
@@ -19,5 +27,13 @@ describe('User Controller', () => {
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
+  });
+
+  it('should return list users', async () => {
+    await expect(userController.findAll()).resolves.toEqual(await userService.findAll());
+  });
+
+  it('should return one user', async () => {
+    await expect(userController.findOne(1)).resolves.toEqual(await userService.findOne(1));
   });
 });
