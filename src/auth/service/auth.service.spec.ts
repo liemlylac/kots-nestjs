1448b55e2a16d4e41194626bc5079a706e0c3fa8
liemlylac <liemlylac@gmail.com>
2020-06-07 22:57:52 +0700

    #9 fix NODE_ENV in test environment

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -6,10 +6,46 @@ import { UserService } from '../../user/service/user.service';
 import { JwtConfigService } from './jwt.config.service';
 import { ConfigModule } from '../../config/config.module';
 import { UserRepository } from '../../user/entity/repo/user.repository';
+import { User } from '../../user/entity/user.entity';
+import { Login } from '../dto/login.dto';
 
-describe('class AuthService', () => {
+describe('AuthService', () => {
 
   let authService: AuthService;
+  let hashService: HashService;
+  let userRepository: UserRepository;
+
+  const testDateData = new Date();
+
+  const login: Login = {
+    username: 'johndoe',
+    password: 'hard!secret-password'
+  }
+
+  const users: User[] = [
+    {
+      id: 'uuid01',
+      displayName: 'John Doe',
+      username: 'johndoe',
+      password: '',
+      email: '',
+      phone: '',
+      active: true,
+      createDate: testDateData,
+      updateDate: testDateData,
+    },
+    {
+      id: 'uuid02',
+      displayName: 'John Doe',
+      username: 'john_doe',
+      password: '',
+      email: '',
+      phone: '',
+      active: false,
+      createDate: testDateData,
+      updateDate: testDateData,
+    },
+  ]
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,10 +62,24 @@ describe('class AuthService', () => {
     }).compile();
 
     authService = module.get<AuthService>(AuthService);
-
+    hashService = module.get<HashService>(HashService);
+    userRepository = module.get<UserRepository>(UserRepository);
+    jest.spyOn(userRepository, 'findOne').mockImplementation(async (condition) => {
+      return users.find(user =>
+        user.username === condition.username && user.active === condition.active
+      );
+    });
+    users[0].password = await hashService.hashPassword(login.password);
+    users[1].password = await hashService.hashPassword(login.password);
   });
 
   it('should be defined', () => {
     expect(authService).toBeDefined();
-  })
+  });
+
+  describe('validateLogin', () => {
+    it('should return User if match username and password', async () => {
+      expect(await authService.validateLogin(login.username, login.password)).toEqual(users[0]);
+    });
+  });
 });
--- src/config/config/auth.config.ts
+++ src/config/config/auth.config.ts
@@ -13,14 +13,14 @@ export function authConfig() {
   return {
     auth: {
       pwd: {
-        pepper: process.env.AUTH_PWD_PEPPER || 'kotsSecretPepper',
-        resetTokenLifeTime: process.env.AUTH_PWD_RESET_TOKEN_LIFE_TIME || 86400000
+        pepper: process.env.AUTH_PWD_PEPPER,
+        resetTokenLifeTime: process.env.AUTH_PWD_RESET_TOKEN_LIFE_TIME
       },
       jwt: {
-        accessSecretKey: process.env.JWT_ACCESS_SECRET_KEY || 'kotsJwtSecretKey',
-        refreshSecretKey: process.env.JWT_REFRESH_SECRET_KEY || 'kotsRefreshJwtSecretKey',
-        accessKeyLifetime: process.env.JWT_ACCESS_KEY_LIFE_TIME || 28800,
-        refreshSecretKeyLifetime: process.env.JWT_REFRESH_SECRET_KEY_LIFE_TIME || 2592000,
+        accessSecretKey: process.env.JWT_ACCESS_SECRET_KEY,
+        refreshSecretKey: process.env.JWT_REFRESH_SECRET_KEY,
+        accessKeyLifetime: process.env.JWT_ACCESS_KEY_LIFE_TIME,
+        refreshSecretKeyLifetime: process.env.JWT_REFRESH_SECRET_KEY_LIFE_TIME,
       }
     }
   }
