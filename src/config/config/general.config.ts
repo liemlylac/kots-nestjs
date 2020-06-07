25279d440059c6af2e9cf397aa78184a41f79021
liemlylac <liemlylac@gmail.com>
2020-06-07 09:40:14 +0700

    #4 Refactor config code as format: Schema first

--- src/config/config/general.config.ts
+++ src/config/config/general.config.ts
@@ -1,14 +1,14 @@
 import * as Joi from '@hapi/joi';
 
+export const generalConfigSchema = {
+  NODE_ENV: Joi.string().valid('development', 'production').default('development'),
+  PORT: Joi.number().default(3000),
+  API_ROOT: Joi.string().default('api'),
+}
+
 export function generalConfig() {
   return {
     port: parseInt(process.env.PORT, 10),
     apiRoot: process.env.API_ROOT,
   }
-}
-
-export const generalConfigSchema = {
-  NODE_ENV: Joi.string().valid('development', 'production').default('development'),
-  PORT: Joi.number().default(3000),
-  API_ROOT: Joi.string().default('api'),
 }
