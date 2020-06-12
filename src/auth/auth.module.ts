f6867d55b80d2fc387060a0f62b027891604d06b
liemlylac <liemlylac@gmail.com>
2020-06-12 23:24:46 +0700

    feat(auth): remove TokenService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,6 @@ import { LocalStrategy } from './service/passport/local.strategy';
 import { AuthService } from './service/auth.service';
 import { HashService } from './service/hash.service';
 import { JwtConfigService } from './service/jwt.config.service';
-import { TokenService } from './service/token.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
@@ -16,13 +15,7 @@ import { UserModule } from '../user/user.module';
     JwtModule.registerAsync({ useClass: JwtConfigService }),
     UserModule,
   ],
-  providers: [
-    AuthService,
-    HashService,
-    TokenService,
-    LocalStrategy,
-    JwtStrategy,
-  ],
+  providers: [AuthService, HashService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService, HashService],
 })
