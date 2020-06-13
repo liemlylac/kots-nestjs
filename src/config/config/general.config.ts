306ffeb117a6cd7de01b3229cc73b86b8f1d4cdf
liemlylac <liemlylac@gmail.com>
2020-06-13 19:45:39 +0700

    chore(config): refactor code

--- src/config/config/general.config.ts
+++ src/config/config/general.config.ts
@@ -1,16 +1,19 @@
 import * as Joi from '@hapi/joi';
 
-export function generalConfig() {
-  return {
-    port: parseInt(process.env.PORT, 10),
-    apiRoot: process.env.API_ROOT,
-  };
-}
-
 export const generalConfigSchema = {
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test')
     .default('development'),
   PORT: Joi.number().default(3000),
   API_ROOT: Joi.string().default('api'),
+  ENABLE_SWAGGER: Joi.boolean().default(false),
 };
+
+export function generalConfig() {
+  return {
+    port: parseInt(process.env.PORT, 10),
+    apiRoot: process.env.API_ROOT,
+    enableSwagger:
+      process.env.ENABLE_SWAGGER.toString().toLowerCase() === 'true',
+  };
+}
